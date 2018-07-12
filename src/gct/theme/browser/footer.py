744447d47c437f7820ad00399e31d3d0f5acd6fd
henry <ah13441673@gmail.com>
2018-07-12 05:58:15 +0000

    add base_inform_configlet

--- src/gct/theme/browser/footer.py
+++ src/gct/theme/browser/footer.py
@@ -3,6 +3,7 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from datetime import date
 from plone import api
 from gct.content.browser.configlet import IDict
+from gct.content.browser.base_inform_configlet import IInform
 import ast
 
 
@@ -23,6 +24,11 @@ class FooterViewlet(FooterViewlet):
                     count+=1
                     subList[subName] = '%s/products?p_category=%s&p_subject=%s' %(abs_url, category, subject)
         fileBrains = api.content.find(path='gct/file_container', portal_type="File", sort_limit=8)
+
+        self.email = api.portal.get_registry_record('email', interface=IInform, default='')
+	self.address = api.portal.get_registry_record('address', interface=IInform, default='')
+	self.cellphone = api.portal.get_registry_record('cellphone', interface=IInform, default='')
+        self.fax = api.portal.get_registry_record('fax', interface=IInform, default='')
 
         self.fileBrains = fileBrains
         self.catList = catList
